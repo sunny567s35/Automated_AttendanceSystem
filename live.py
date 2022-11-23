1656a08d254f618b730836a730a6307f064b4154
sunny567s35 <sunny567s35@gmail.com>
2022-11-23 11:33:30 +0530

    added fps and number of upscales

--- live.py
+++ live.py
@@ -9,17 +9,19 @@ import pickle
 # get default video FPS
 # fps = cap.get(cv2.CAP_PROP_FPS)
  
-# # get total number of video frames
+# # get total number of video framesqq
 # num_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 
 # print(dlib.DLIB_USE_CUDA)
 #---------------------------------------------------------------------------------------------
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 data = pickle.loads(open('face_encodings',"rb").read())
 nameList = []
 print(nameList)
-
+#-----------------------------------------------------------added FPS counter
+print("FPS : {:0.3}".format(cap.get(cv2.CAP_PROP_FPS)))
+#=============================================================
 def markAttendance(name):
     with open('Attendance.csv', 'a') as f:
         
@@ -36,9 +38,10 @@ while True:
 # img = captureScreen()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
-
-    facesCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+    #------------------------------added number of upsamples
+    facesCurFrame = face_recognition.face_locations(imgS,number_of_times_to_upsample=2)
+    #------------------------------added number of jitters
+    encodesCurFrame = face_recognition.face_encodings(imgS,known_face_locations= facesCurFrame)
 
     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
         mat = False
@@ -48,7 +51,7 @@ while True:
         # print(matches)
         matchIndex = np.argmin(faceDis)
         
-        if(faceDis[matchIndex] <= 0.45):
+        if(faceDis[matchIndex] <= 0.47):
             
             mat = True
 
@@ -64,9 +67,15 @@ while True:
         cv2.rectangle(img, (x, y1), (x2, y2), (0, 255, 0), 2)
         cv2.rectangle(img, (x, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
         cv2.putText(img, name, (x + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-        if(name!='Buchodu'):
+        if(name!='Buchodu!'):
             markAttendance(name)
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
+     # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap().release()
+cv2.destroyAllWindows()
     
